82a012f6649921d64a56b00adffa43563c93a758
= <berkayengin56@gmail.com>
2023-02-11 17:41:16 +0300

    Upload menu_pic and get_pic url as return yapıldı. save menu info to database yolda !!!

--- lib/uploadScreens/menu_upload_screen.dart
+++ lib/uploadScreens/menu_upload_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:cakery_repo/global/global.dart';
 import 'package:cakery_repo/mainScreens/home_screen.dart';
+import 'package:cakery_repo/widgets/error_dialog.dart';
+import 'package:cakery_repo/widgets/progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:firebase_storage/firebase_storage.dart' as storageRef;
 
 
 
@@ -18,6 +22,13 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
 
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
+
+  TextEditingController shortInfoController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+
+  bool uploading = false;
+  String uniqueIdName = DateTime.now().millisecondsSinceEpoch.toString();
+
 
   defaultScreen(){
     return Scaffold(
@@ -168,9 +179,211 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
 
   }
 
+  menusUploadFormScreen(){
+    return Scaffold(
+        appBar: AppBar(
+          flexibleSpace:Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white54,
+                    Colors.grey,
+                  ],
+                  begin: const FractionalOffset(0.0, 0.0),
+                  end: const FractionalOffset(2.0, 2.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp,
+                )
+            ),
+          ) ,
+          title: const Text(
+            // sellerın yeni menü eklemesini sağlıyor
+            "Uploading New Menu",
+            style: TextStyle(fontSize: 15, fontFamily: "Lobster"),
+          ),
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white,),
+            onPressed: (){
+
+
+              clearMenusUploadForm();
+            },
+          ),
+          actions: [
+            TextButton(
+                child: const Text(
+                  // add butonuna tıkladığımızda yapabileceğimiz aksiyonları gösterdiğimiz kısım
+                 "Add",
+                 style: TextStyle(
+                   color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                   fontSize: 18,
+                   fontFamily: "Varela",
+                   letterSpacing: 3,
+                 ),
+                ),
+              onPressed: uploading ? null: ()=> validateUploadForm(),
+              // we are cheking that if uploading is true there and doing nothing means nothing else do validateUploadForm
+            ),
+          ],
+        ),
+      body: ListView(
+        children: [
+          uploading == true ? linearProgress() : const Text(""),
+          Container(
+            height: 230,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16/9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(
+                          File(imageXFile!.path)
+                      ),
+                      fit: BoxFit.cover,
+                    )
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            // çizgi ekler araya
+            color: Colors.pink,
+            thickness: 1,
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.perm_device_information, color: Colors.white,),
+            title: Container(
+              width: 250,
+              child: TextField(
+                // menu bilgisinin girildiği kısım
+                style: const TextStyle(color: Colors.black),
+                controller: shortInfoController,
+                  decoration: const InputDecoration(
+                    hintText: "Menu info",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                  ),
+
+              ),
+          ),
+          ),
+          const Divider(
+            // çizgi ekler araya
+            color: Colors.pink,
+            thickness: 1,
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.title, color: Colors.white,),
+            title: Container(
+              // menu adının girildiği kısım
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.black),
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: "Menu title",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                ),
+
+              ),
+            ),
+          ),
+          const Divider(
+            // çizgi ekler araya
+            color: Colors.pink,
+            thickness: 1,
+          ),
+        ],
+      ),
+
+
+    );
+  }
+
+  clearMenusUploadForm(){
+
+    setState(() {
+      shortInfoController.clear();
+      titleController.clear();
+      imageXFile = null;
+    });
+
+  }
+  validateUploadForm() async{
+    setState(() {
+      uploading = true;
+    });
+    if(imageXFile != null){
+
+      if(shortInfoController.text.isNotEmpty && titleController.text.isNotEmpty){
+
+        setState(() {
+          uploading = true;
+        });
+        // upload image
+         String downloadURL = await uploadImage(File(imageXFile!.path));
+        // save info to firebase
+
+      }
+      else{
+        showDialog(
+            context: context,
+            builder: (c){
+              return ErrorDialog(
+                // eğer title ve info girmezse bu hata çıkıcak
+                message: "Please write title and info for Menu" ,
+              );
+            }
+        );
+
+      }
+
+    }
+    else{
+      showDialog(
+          context: context,
+          builder: (c){
+            return ErrorDialog(
+              // eğer bir fotoğraf yüklemezse menüye bu hata çıkıcak
+              message: "Please pick an picture for Menu" ,
+            );
+          }
+      );
+
+
+    }
+
+  }
+  uploadImage(mImageFile) async{
+    // WE cleared a reference to infer storage and inside the menus folder
+    // we want to put our file, reference to our child
+    //we serve that reference and references to the menus
+    // we have to basically get the download, which we cant get using the task snapshot
+    // we implement that and using the task snapshot, we get the download URL of the uploaded image
+    //and then we return the download URL
+    storageRef.Reference reference = storageRef.FirebaseStorage
+        .instance
+        .ref()
+        .child("menus");
+    storageRef.UploadTask uploadTask= reference.child(uniqueIdName + ".jpg").putFile(mImageFile);
+
+    storageRef.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
+    String downloadURL = await taskSnapshot.ref.getDownloadURL();
+    return downloadURL;
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return defaultScreen();
+    return imageXFile == null ? defaultScreen() : menusUploadFormScreen();
   }
 }
