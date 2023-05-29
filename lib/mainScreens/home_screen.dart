b12f83abefad4a3a97bd158b8c763bcb603b168e
= <berkayengin56@gmail.com>
2023-05-29 22:18:45 +0300

    seller app bazı tasarım hataları düzeltildi !

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -21,6 +21,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
 class _HomeScreenState extends State<HomeScreen> {
 
   restrictBlockedSellerFromUsingApp() async {
